bc90626d9bbd200739f81ec25479aa4abee83dbb
giovanni <giovanni@giovanni-X550JX>
2017-10-20 11:56:34 +0100

    Kata3 -- Third Version

--- NetBeansProjects/kata3/src/kata3/Kata3.java
+++ NetBeansProjects/kata3/src/kata3/Kata3.java
@@ -5,27 +5,11 @@ public class Kata3 {
     public static void main(String[] args) {
         //Propuesta versión 4: Reducir el uso de increment para sólo usar 1 por dominio (usando dos parámetros de entrada)
         Histogram histogram = new Histogram();
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment(1);
-        histogram.increment(1);
-        histogram.increment(1);
-        histogram.increment(1);
-        histogram.increment(1);
+        histogram.increment("hotmail.com", 10);
+        histogram.increment("ulpgc.es", 7);
+        histogram.increment("ull.es", 5);
+        histogram.increment("gmail.com", 12);
+        
         new HistogramDisplay(histogram).execute();
         
     }
