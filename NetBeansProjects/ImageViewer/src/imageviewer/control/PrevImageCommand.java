9954ebc44cc9e84d53588413ca63e541260a1cff
giovanni <giovanni@giovanni-X550JX>
2018-01-18 17:50:05 +0000

    ImageViewer

---
+++ NetBeansProjects/ImageViewer/src/imageviewer/control/PrevImageCommand.java
@@ -0,0 +1,21 @@
+package imageviewer.control;
+
+import imageviewer.ui.ImageDisplay;
+
+public class PrevImageCommand implements Command{
+    private final ImageDisplay imageDisplay;
+
+    public PrevImageCommand(ImageDisplay imageDisplay) {
+        this.imageDisplay = imageDisplay;
+    }
+
+    @Override
+    public String name() {
+        return "⇦";
+    }
+
+    @Override
+    public void execute() {
+        imageDisplay.display(imageDisplay.currentImage().prev());
+    }
+}
